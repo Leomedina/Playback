d3700193cd633e585e0f2cdbec55dc0552a598a1
Leo <medinayleo@gmail.com>
2021-03-01 18:26:17 -0500

    working on podcast slot styling

--- src/components/PodcastSlot/index.js
+++ src/components/PodcastSlot/index.js
@@ -9,8 +9,20 @@ function PodcastSlot({ id, podcast, index }) {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          className="p-4 mb-3 flex justify-between items-center bg-green-400 shadow rounded-lg">
-          {podcast.name}
+          className="p-4 mb-3 max-h-50 flex bg-green-400 shadow rounded-md">
+          <div className="flex-1">
+            <h1>LOGO</h1>
+          </div>
+          <div className="flex-2 px-10 justify-start">
+            <h1>{podcast.title}</h1>
+            <h1>{podcast.name}</h1>
+            <p className="">
+              {podcast.description}
+            </p>
+          </div>
+          <div className="flex-1">
+            PLAYBUTTON
+          </div>
         </div>
       )}
     </Draggable>
